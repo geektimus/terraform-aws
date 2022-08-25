caeb8cbd5c670afa6e2cb1a3a02e6215ad78e688
Geektimus <alexander.kno+github@gmail.com>
2022-08-25 16:30:43 -0500

    First attemp to deploy

--- main.tf
+++ main.tf
@@ -9,11 +9,151 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = ""
-  secret_key = ""
 }
 
 # Create VPC
-resource "aws_vpc" "alfheim-vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "production_vpc_0" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name : "production"
+  }
+}
+
+resource "aws_internet_gateway" "production_gw" {
+  vpc_id = aws_vpc.production_vpc_0.id
+
+  tags = {
+    Name = "production"
+  }
+}
+
+resource "aws_route_table" "prod_route_table_0" {
+  vpc_id = aws_vpc.production_vpc_0.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.production_gw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.production_gw.id
+  }
+
+  tags = {
+    Name = "production"
+  }
+}
+
+variable "subnet_prefix" {
+  description = "cidr block for the subnet"
+  type = string
+}
+
+resource "aws_subnet" "production_subnet_0" {
+  vpc_id            = aws_vpc.production_vpc_0.id
+  cidr_block        = var.subnet_prefix
+  availability_zone = "us-east-1a"
+  tags = {
+    Name : "production"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.production_subnet_0.id
+  route_table_id = aws_route_table.prod_route_table_0.id
+}
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.production_vpc_0.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "NodeJs from VPC"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+resource "aws_network_interface" "web_server_nic" {
+  subnet_id       = aws_subnet.production_subnet_0.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+}
+
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.web_server_nic.id
+  associate_with_private_ip = "10.0.1.50"
+
+  depends_on = [
+    aws_internet_gateway.production_gw
+  ]
+}
+
+output "pihole_public_ip" {
+  value = aws_eip.one.public_ip
+}
+
+resource "aws_instance" "pihole" {
+  ami               = "ami-052efd3df9dad4825"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name          = "valhala_keypair"
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.web_server_nic.id
+  }
+
+  tags = {
+    Name = "valhala_web"
+  }
+
+  user_data = <<-EOF
+              #!/bin/env bash
+              sudo apt update -y
+              sudo apt upgrade -y
+              sudo apt install unzip unbound -y
+              EOF
 }
